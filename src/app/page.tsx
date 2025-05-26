9b7aa26a809e31187fbe565d212b3e94551995cb
jakedahn <jake@shruggingface.com>
2025-05-26 13:10:45 -0600

    Reverting last fix, and downgrading react-date-picker to v8 to fix calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
-  return <div className="home">hello</div>;
+  return (
+    <div className="home">
+      <Calendar mode="single" className="rounded-md border shadow" />
+    </div>
+  );
 }
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DayPickerProps } from "react-day-picker";
+import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 
 function Calendar({
   className,
@@ -59,28 +59,17 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={
-        {
-          IconLeft: ({
-            className,
-            ...props
-          }: {
-            className?: string;
-            props?: React.SVGProps<SVGSVGElement>;
-          }) => <ChevronLeft className={cn("size-4", className)} {...props} />,
-
-          IconRight: ({
-            className,
-            ...props
-          }: {
-            className?: string;
-            props?: React.SVGProps<SVGSVGElement>;
-          }) => <ChevronRight className={cn("size-4", className)} {...props} />,
-        } as unknown as Partial<DayPickerProps["components"]>
-      }
+      components={{
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeft className={cn("size-4", className)} {...props} />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRight className={cn("size-4", className)} {...props} />
+        ),
+      }}
       {...props}
     />
-  );
+  )
 }
 
 export { Calendar }
